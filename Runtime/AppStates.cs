b55d4f32a7230e24f43301dc6b3a18891385fc13
zilch <wlgys8@gmail.com>
2020-01-16 21:06:05 +0800

    add onPause event

--- Runtime/AppStates.cs
+++ Runtime/AppStates.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace MS.CommonUtils{
     public class AppStates : MonoBehaviour
     {
         
+        public static event UnityAction<bool> onPause;
         public static bool quiting{
             get;private set;
         }
+
         private void OnApplicationQuit() {
             quiting = true;
+        }
+
+        void OnApplicationPause(bool paused){
+            if(onPause != null){
+                onPause(paused);
+            }
         }
 
 
