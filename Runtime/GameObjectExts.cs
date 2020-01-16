da7d053411d6826ea809f3cd43cb6310cd635e29
zilch <wlgys8@gmail.com>
2020-01-16 21:05:32 +0800

    add GameObjectExt

---
+++ Runtime/GameObjectExts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MS.CommonUtils
+{
+    
+    public static class GameObjectExts
+    {
+        
+        public static void SetLayerRecursively(this GameObject go,int layer){
+            go.layer = layer;
+            for(var i = 0; i < go.transform.childCount;i++){
+                go.transform.GetChild(i).gameObject.SetLayerRecursively(layer);
+            }
+        }
+    }
+}
