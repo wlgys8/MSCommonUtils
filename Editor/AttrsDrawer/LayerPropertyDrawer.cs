bc2780139fb097ed69279a36f52ab922138d22a7
zilch <wlgys8@gmail.com>
2020-06-06 13:24:49 +0800

    增加LayerFieldAttribute

---
+++ Editor/AttrsDrawer/LayerPropertyDrawer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace MS.CommonUtils.Editor{
+    [CustomPropertyDrawer(typeof(LayerFieldAttribute))]
+    public class LayerPropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label){
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Enum,label);
+        }
+
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.Integer)
+            {
+                Debug.LogError("Layer property should be an integer ( the layer id )");
+            }
+            else
+            {
+                property.intValue = EditorGUI.LayerField(position,label,property.intValue);
+            }
+        }
+    }
+}
---
+++ Runtime/Attrs/LayerFieldAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace MS.CommonUtils
+{
+
+    /// <summary>
+    /// 加载int字段上，标记为layer。 在Insepctor即可以显示为layer控件
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public class LayerFieldAttribute : PropertyAttribute{}
+}
