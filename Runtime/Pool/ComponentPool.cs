6d10cd769e1c6a3d3502bb34330bb0d0358d860b
zilch <wlgys8>
2020-08-31 21:18:56 +0800

    Refactor quitting check

--- Runtime/Pool/ComponentPool.cs
+++ Runtime/Pool/ComponentPool.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace MS.CommonUtils{
+
+
+    /// <summary>
+    /// Designed for storing Component objects.
+    /// All objects released into the pool will be the child of poolNode,which won't be destroyed on scene unload.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class ComponentPool<T> :ObjectPool<T> where T:Component
     {
         
         private string _name;
+
         public ComponentPool(string name){
             _name = name;
         }
@@ -15,7 +22,7 @@ namespace MS.CommonUtils{
         private Transform _poolNode;
         public Transform poolNode{
             get{
-                if(AppStates.quiting){
+                if(AppQuittingCheck.quitting){
                     return null;
                 }
                 if(!_poolNode){
@@ -27,18 +34,54 @@ namespace MS.CommonUtils{
             }
         }
 
+        /// <summary>
+        /// Remove all items from the pool, and call Object.Destroy on it's gameObject.
+        /// </summary>
+        public void ClearAndDestroyAll(){
+            while(Count > 0){
+                var ret = Request();
+                Object.Destroy(ret.gameObject);
+            }
+        }
 
-        public override T Request(){
+
+        public T Request(Transform parent){
             T ret = base.Request();
+            ret.transform.SetParent(parent,false);
             return ret;
         }
 
+        public override T Request(){
+            return Request(null);
+        }
+
         public override void Release(T item){
+            var p = poolNode;
+            if(!item){
+                return;
+            }
             base.Release(item);
             item.transform.SetParent(poolNode,false);
         }
-
-
         
     }
+
+    internal static class AppQuittingCheck{
+        public static bool quitting{
+            get{
+                return _quiting;
+            }
+        }
+        private static void OnApplicationQuit(){
+            _quiting = true;
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void InitializeOnLoad(){
+            Application.quitting += OnApplicationQuit;
+        }
+
+        private static bool _quiting = false;        
+    }
+
 }
